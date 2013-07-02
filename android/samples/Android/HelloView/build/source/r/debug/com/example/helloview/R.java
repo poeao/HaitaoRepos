7d0bff63599a02689a3cacbdde5d8a018c71224f
haitao.song <haitao006@gmail.com>
2013-07-02 10:32:44 +0800

    remove yaya project

--- android/samples/Android/HelloView/build/source/r/debug/com/example/helloview/R.java
+++ android/samples/Android/HelloView/build/source/r/debug/com/example/helloview/R.java
@@ -18,13 +18,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
-        public static final int button=0x7f080002;
-        public static final int checkBox=0x7f080006;
-        public static final int editText=0x7f080001;
-        public static final int radioButton1=0x7f080005;
-        public static final int radioButton2=0x7f080004;
-        public static final int radioGroup=0x7f080003;
+        public static final int about=0x7f08000b;
+        public static final int action_settings=0x7f08000a;
+        public static final int button=0x7f080003;
+        public static final int checkBox=0x7f080004;
+        public static final int editText=0x7f080002;
+        public static final int exit=0x7f08000c;
+        public static final int linearLayout=0x7f080001;
+        public static final int progressBar=0x7f080009;
+        public static final int radioButton1=0x7f080007;
+        public static final int radioButton2=0x7f080006;
+        public static final int radioGroup=0x7f080005;
+        public static final int spinner=0x7f080008;
         public static final int textView=0x7f080000;
     }
     public static final class layout {
@@ -32,6 +37,7 @@ public final class R {
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int mainmenu=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050000;
--- android/samples/Android/HelloView/src/main/java/com/example/helloview/MainActivity.java
+++ android/samples/Android/HelloView/src/main/java/com/example/helloview/MainActivity.java
@@ -1,23 +1,28 @@
 package com.example.helloview;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.app.Activity;
-import android.util.Log;
-import android.view.Menu;
-import android.view.View;
+import android.view.*;
 import android.widget.*;
 
 public class MainActivity extends Activity {
 
+    String[] cities = {"上海","北京","珠海","承德"};
+    ArrayAdapter<String> adapter = null;
+    boolean display = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button button = (Button)findViewById(R.id.button );
+        Button button2 = (Button)findViewById(R.id.button2 );
         RadioButton rb = (RadioButton)findViewById(R.id.radioButton1);
         RadioGroup rg = (RadioGroup)findViewById(R.id.radioGroup);
         CheckBox cb = (CheckBox) findViewById(R.id.checkBox);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner );
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,8 +30,25 @@ public class MainActivity extends Activity {
                 EditText text = (EditText)findViewById(R.id.editText );
                 Button button = (Button)view;
                 button.setText("Clicked Button");
-                show(text.getText().toString());
+                if(text.getText() != null) {
+                    show(text.getText().toString());
+                }
                 text.setText("Changed By Click Button");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressBar bar = (ProgressBar)findViewById(R.id.progressBar );
+                if(display) {
+                    bar.setVisibility(View.INVISIBLE);
+                    display = false;
+                } else {
+                    bar.setVisibility(View.VISIBLE);
+                    display = true;
+                }
+
             }
         });
 
@@ -34,8 +56,8 @@ public class MainActivity extends Activity {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if(b) {
-                    RadioButton r = (RadioButton)compoundButton;
-                    show("Radio Button has been clicked." + r.getText().toString());
+                   // RadioButton r = (RadioButton)compoundButton;
+                    //show("Radio Button has been clicked."));
                 }
             }
         });
@@ -62,17 +84,59 @@ public class MainActivity extends Activity {
                 }
             }
         });
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cities);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                String text = ((TextView)view).getText().toString() +"position : " + position +" with ID --> " + id;
+                show(text);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                show("You 啥也没选啊!好赖选一个呗!");
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-       getMenuInflater().inflate(R.menu.main, menu);
+       getMenuInflater().inflate(R.menu.mainmenu, menu);
+
+        menu.add("Main");
         return true;
     }
 
     private void show(String msg) {
         Toast.makeText(this, msg, Toast.LENGTH_SHORT ).show();
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.exit) {
+            finish();
+        } else if(item.getItemId() == R.id.about) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Title");
+            AlertDialog dialog = builder.create();
+            dialog.setMessage("Hello, 欢迎点击菜单!");
+            dialog.setButton(DialogInterface.BUTTON_POSITIVE, "Close", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+
+            dialog.show();;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
